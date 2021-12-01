006f924f545eb27207f76cc310441577bdfa8d90
calmohtml <contactosegovia@protonmail.com>
2021-12-01 18:34:20 -0300

    First commit

---
+++ pages/_app.jsx
@@ -0,0 +1,51 @@
+import { Fragment } from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --titles: 'Outfit', sans-serif; 
+    --texts: 'Cabin', sans-serif;
+
+    --dark1:#2e3440;
+    --dark2:#3b4252;
+    --dark3: #434c5e;
+    --dark4:#4c566a;
+    --white: #e9e9e9;
+  }
+
+  *, 
+  *::before, 
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: var(--texts);
+    color: var(--white)
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: var(--titles);
+  }
+
+  #__next {
+    margin: 0 auto;
+    width: 90%;
+    min-height: 100vh;
+  }
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Fragment>
+  );
+}
